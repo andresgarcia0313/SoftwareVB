15fa4c6ce594da0e766f2a61c2bdb2428f10030c
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2024-07-20 22:40:28 -0500

    Versión inicial de depuración en Visual Basic: Se ha implementado una configuración que genera automáticamente un punto de interrupción en la primera línea del código. Sin embargo, los puntos de interrupción en Visual Studio Code no se están aplicando de forma estándar y deben ser añadidos manualmente con la palabra stop en el código. Aquí están ya configurados el launch.json y tasks.json donde launch esta configurada la depuración y tasks.json se configura la compilación

---
+++ Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Bienvenido")
+        Console.WriteLine("Hola!")
+        Stop
+        Console.WriteLine("Adios")
+    End Sub
+End Module
